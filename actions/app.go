3fb8f6e5446faf3d779d25a32e85355c2b49c0cb
Yonghwan SO <scinix@gmail.com>
2023-01-30 02:35:23 +0900

    coke webapp generated with buffalo v0.18.3

--- actions/app.go
+++ actions/app.go
@@ -20,8 +20,11 @@ import (
 // ENV is used to help switch settings based on where the
 // application is being run. Default is "development".
 var ENV = envy.Get("GO_ENV", "development")
-var app *buffalo.App
-var T *i18n.Translator
+
+var (
+	app *buffalo.App
+	T   *i18n.Translator
+)
 
 // App is where all routes and middleware for buffalo
 // should be defined. This is the nerve center of your
